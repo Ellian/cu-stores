4b5b3fbfcd4be8765fcfbebfc2f86760108019cb
Mehuge <adf@sorcerer.co.uk>
2015-08-23 13:08:36 +0100

    Not using eventemitter3

--- src/definitions/tsd.d.ts
+++ src/definitions/tsd.d.ts
@@ -1,5 +1,4 @@
 /// <reference path="react/react.d.ts" />
 /// <reference path="reflux/reflux.d.ts" />
-/// <reference path="eventemitter3/eventemitter3.d.ts" />
 /// <reference path="../../node_modules/cu-core/lib/cu-core.d.ts" />
 /// <reference path="../../node_modules/cu-events/lib/cu-events.d.ts" />
